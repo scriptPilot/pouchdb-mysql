95824bebc18da4cd5694b4f271e46a85f7de955c
scriptPilot <dennis@scheffel.de>
2024-02-19 21:28:20 +0100

    testing with Jest (wip)

--- demo-app/testdata.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO `tasks` (`id`, `title`, `done`)
-VALUES (1, "First Task", 1), (2, "Second Task", 0), (3, "Third Task", 1);
--- demo-app/vite.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  server: {
-    proxy: {
-      // eslint-disable-next-line no-useless-escape
-      '^(.+)\\.php': 'http://localhost:8000/',
-    },
-  },}
