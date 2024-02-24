8404509de25ec8c9be725113e0c4fd3616e4909b
scriptPilot <dennis@scheffel.de>
2024-02-24 21:59:54 +0100

    remove old tests

--- tests/index.test.js
+++
@@ -1,8 +0,0 @@
-import index from '../lib/index'
-import useDB from '../lib/useDB'
-
-describe('index file', () => {
-  test('returns the useDB() function', () => {
-    expect(index).toEqual(useDB)
-  })
-})
